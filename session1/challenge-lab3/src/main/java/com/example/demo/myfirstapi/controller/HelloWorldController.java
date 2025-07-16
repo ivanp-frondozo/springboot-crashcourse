4be3dc617f35e529c6cb19272825dd9a8797f3f3
Ivan Frondozo <ivan-p.frondozo@gcash.com>
2025-07-16 14:57:26 +0800

    Commented out other lab functionalities

--- session1/challenge-lab3/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
+++ session1/challenge-lab3/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -11,29 +11,29 @@ import java.util.*;
 @RestController // Marks this class as a REST controller
 public class HelloWorldController {
 
-    @Value("${app.greeting.message}")
-    private String customGreeting;
-
-    @GetMapping("/hello") // Maps HTTP GET requests to the "/hello" URL
-    public String sayHello() {
-        return customGreeting;
-        //return "Hello, Spring Boot gitLearners!";
-    }
-
-    @GetMapping("/greet/{name}") // This path now expects a dynamic 'name' part
-    public String greetUser(@PathVariable String name) { // @PathVariable maps the URL part to this parameter
-        return "Greetings, " + name + "!";
-    }
-
-    @GetMapping("/greeting")
-    public String greetWithParam(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-        return "Hello, " + name + "!";
-    }
-    @GetMapping("/sum")
-    public String sumWithParam(@RequestParam(value="int1") int int1, @RequestParam(value="int2") int int2) {
-        int sum = int1 + int2;
-        return "The sum of " + int1 + " and " + int2 + " is " + sum + ".";
-    }
+//    @Value("${app.greeting.message}")
+//    private String customGreeting;
+//
+//    @GetMapping("/hello") // Maps HTTP GET requests to the "/hello" URL
+//    public String sayHello() {
+//        return customGreeting;
+//        //return "Hello, Spring Boot gitLearners!";
+//    }
+//
+//    @GetMapping("/greet/{name}") // This path now expects a dynamic 'name' part
+//    public String greetUser(@PathVariable String name) { // @PathVariable maps the URL part to this parameter
+//        return "Greetings, " + name + "!";
+//    }
+//
+//    @GetMapping("/greeting")
+//    public String greetWithParam(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+//        return "Hello, " + name + "!";
+//    }
+//    @GetMapping("/sum")
+//    public String sumWithParam(@RequestParam(value="int1") int int1, @RequestParam(value="int2") int int2) {
+//        int sum = int1 + int2;
+//        return "The sum of " + int1 + " and " + int2 + " is " + sum + ".";
+//    }
 
     @GetMapping("/info")
     public Map<String, String> getAppInfo() {
