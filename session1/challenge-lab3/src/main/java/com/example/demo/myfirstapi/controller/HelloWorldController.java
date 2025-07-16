abcf6b4520919d6c8130b6b24f34be8b7f10b305
Ivan Frondozo <ivan-p.frondozo@gcash.com>
2025-07-16 14:29:27 +0800

    Completed functionality for /features and /info

--- session1/challenge-lab3/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
+++ session1/challenge-lab3/src/main/java/com/example/demo/myfirstapi/controller/HelloWorldController.java
@@ -29,4 +29,24 @@ public class HelloWorldController {
     public String greetWithParam(@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
         return "Hello, " + name + "!";
     }
+    @GetMapping("/sum")
+    public String sumWithParam(@RequestParam(value="int1") int int1, @RequestParam(value="int2") int int2) {
+        int sum = int1 + int2;
+        return "The sum of " + int1 + " and " + int2 + " is " + sum + ".";
+    }
+
+    @GetMapping("/info")
+    public Map<String, String> getAppInfo() {
+        Map<String, String> info = new HashMap<>();
+        info.put("appName", "MyFirstAPIDemo");
+        info.put("version", "1.0.0");
+        info.put("status", "running");
+        return info;
+    }
+
+    @GetMapping("/features")
+    public List<String> getFeature() {
+        return Arrays.asList("REST API", "Spring Boot", "Fast Development");
+    }
 }
+
